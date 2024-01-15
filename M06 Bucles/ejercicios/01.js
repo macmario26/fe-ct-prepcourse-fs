47dba2df1e5e85f203d081de1532d65224d1e7f1
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-15 11:27:34 -0300

    Update 01.js
    Tarea 01 cumplimentada

--- M06 Bucles/ejercicios/01.js
+++ M06 Bucles/ejercicios/01.js
@@ -1,17 +1,16 @@
+var a = 4;
+var b = -4;
 function esIgualYNegativo(a, b) {
   // La función recibe dos argumentos "a" y "b" que son números.
   // Determina si son iguales y son ambos negativos.
   // De ser así, retorna true, de lo contrario, retorna false.
   // Tu código:
+ 
+  if(a == b && (a<0 && b<0)){
+    console.log(true)
+  }else{
+    console.log(false)
+}
 }
 
 module.exports = esIgualYNegativo;
-
-var a = 4;
-var b = -4;
- 
-if(a == b && (a<0 && b<0)){
-  console.log(true)
-}else{
-  console.log(false)
-}
