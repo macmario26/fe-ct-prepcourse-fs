55862a2d63d9bd877b9087e33c07f3662604b73d
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:52:53 -0300

    Update 08.js

--- M06 Bucles/ejercicios/08.js
+++ M06 Bucles/ejercicios/08.js
@@ -3,6 +3,15 @@ function esPositivoOInferiorA10(a) {
    // Retorna true si es positivo y menor que 10.
    // Retorna false en caso contrario.
    // Tu código:
+   if (a>0 && a<10) {
+      return true
+   }else{return false}
 }
 
+var respuesta1 = esPositivoOInferiorA10(2)
+console.log(respuesta1)
+var respuesta2 = esPositivoOInferiorA10(20)
+console.log(respuesta2)
+var respuesta3 = esPositivoOInferiorA10(-2)
+console.log(respuesta3)
 module.exports = esPositivoOInferiorA10;
