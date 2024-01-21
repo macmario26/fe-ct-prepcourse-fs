b73b5c2ef69895993980af1c0ed5ed11818c2030
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 18:00:37 -0300

    Update 22.js

--- M06 Bucles/ejercicios/22.js
+++ M06 Bucles/ejercicios/22.js
@@ -3,6 +3,15 @@ function diasEnMes(mes) {
   // Determine cúantos días tiene el mes correspondiente a ese número.
   // PISTA: Usa un bucle do-while para validar que el mes ingresado sea válido.
   // Tu código:
-}
 
+  do {
+    if (0 > mes > 12) {
+      return('El valor ingresado no corresponde a un mes calendario')
+    }
+  } while (0 > mes > 12){
+    return 'no pue'
+  };
+}
+let calendario1 = diasEnMes(13)
+console.log(calendario1)
 module.exports = diasEnMes;
