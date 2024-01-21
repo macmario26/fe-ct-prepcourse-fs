3a14ee17206b7a339c08432970e5154b31bb7cee
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:55:23 -0300

    Update 12.js

--- M06 Bucles/ejercicios/12.js
+++ M06 Bucles/ejercicios/12.js
@@ -4,6 +4,29 @@ function fizzBuzz(num) {
   // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
   // De lo contrario, retorna false.
   // Tu código:
+  switch (true) {
+    case num % 3 === 0 && num % 5 === 0:
+      return 'fizzbuzz'
+      break;
+    case num % 3 === 0:
+      return 'fizz'
+      break;
+    case num % 5 === 0:
+      return 'buzz'
+      break;  
+    default: 
+    return false
+      break;
+  }
 }
+
+let giveMe1 = fizzBuzz(10)
+console.log(giveMe1)
+let giveMe2 = fizzBuzz(9)
+console.log(giveMe2)
+let giveMe3 = fizzBuzz(30)
+console.log(giveMe3)
+let giveMe4 = fizzBuzz(22)
+console.log(giveMe4)
 
 module.exports = fizzBuzz;
