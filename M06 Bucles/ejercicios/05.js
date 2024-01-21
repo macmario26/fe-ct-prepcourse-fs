e65d681cffb370c62ddb730cadb4c2329e9e06e8
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:51:04 -0300

    Update 05.js

--- M06 Bucles/ejercicios/05.js
+++ M06 Bucles/ejercicios/05.js
@@ -1,7 +1,15 @@
+var si;
 function esVerdadero(valor) {
   // Si "valor" es verdadero, retorna "Soy verdadero".
   // Caso contrario, retorna "Soy falso".
   // Tu código:
+   if (valor == si) {
+        console.log('soy verdadero')
+    } else {
+        console.log('soy falso');
+    }
 }
+
+esVerdadero(si)
 
 module.exports = esVerdadero;
