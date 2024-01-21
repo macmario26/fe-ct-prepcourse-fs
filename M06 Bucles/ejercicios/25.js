917a4f59dee882a488fb101493a1abc420536aa9
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 18:02:00 -0300

    Update 25.js

--- M06 Bucles/ejercicios/25.js
+++ M06 Bucles/ejercicios/25.js
@@ -5,6 +5,38 @@ function esPalindromo(string) {
   // IMPORTANTE: Un palíndromo es una palabra o frase
   // que se lee igual hacia adelante que hacia atrás.
   // Tu código:
+
+  let cc = string.length;
+  console.log('Longitud de la cadena: ' + cc); 
+
+  let esPalindromo = true;
+
+  for (let i = 0; i < cc; i++) {
+    const charDirecto = string[i];
+    const charInverso = string[cc - 1 - i];
+
+    console.log(charDirecto, charInverso);
+
+    if (charDirecto !== charInverso) {
+      esPalindromo = false;
+      break;
+    }
+  }
+
+  if (esPalindromo) {
+    console.log('Es PALINDROMO');
+  } else {
+    console.log('No es Palindromo');
+  }
+
 }
 
+esPalindromo('neuquen')
+console.log('..........')
+esPalindromo('aldana')
+console.log('..........')
+esPalindromo('itati')
+console.log('..........')
+esPalindromo('lavan naval')
 module.exports = esPalindromo;
+
