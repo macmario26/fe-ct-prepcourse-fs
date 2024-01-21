ef685ebe7343179f6edf4f8277fc3e2e8f3e00aa
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:54:07 -0300

    Update 10.js

--- M06 Bucles/ejercicios/10.js
+++ M06 Bucles/ejercicios/10.js
@@ -2,6 +2,14 @@ function esDiezOCinco(num) {
   // Retornar true si "num" es 10 o 5.
   // De lo contrario, retornar false.
   // Tu código:
+  if (num === 5 || num === 10) {
+    return true
+  } else{ return false }
 }
 
-module.exports = esDiezOCinco;
+let respuesta1 = esDiezOCinco(5)
+  console.log(respuesta1)
+  let respuesta2 = esDiezOCinco(10)
+  console.log(respuesta2)
+  let respuesta3 = esDiezOCinco(3)
+  console.log(respuesta3)
