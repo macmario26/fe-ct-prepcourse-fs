4b9ce02d6c5f739af7e4566798042c4a911abad0
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:59:55 -0300

    Update 21.js

--- M06 Bucles/ejercicios/21.js
+++ M06 Bucles/ejercicios/21.js
@@ -4,6 +4,27 @@ function esPotenciaDeDos(numero) {
   // Devuelve true si lo es, sino devuelve false.
   // PISTA: Utiliza un bucle while.
   // Tu código:
+  if (numero <= 0) { //Primero comprobamos que le número ingresado no sea negativo ni cero.
+    return false;
+  }
+
+  while (numero > 1) {
+    if (numero % 2 !== 0) { //Este if comprueba que el número ingresado sea divisible por 2.
+      return false
+    }
+    numero/=2 
+  }
+  return true 
 }
 
+let respuesta1 = esPotenciaDeDos (5)
+console.log(respuesta1)
+let respuesta2 = esPotenciaDeDos (256)
+console.log(respuesta2)
+let respuesta3 = esPotenciaDeDos (20)
+console.log(respuesta3)
+let respuesta4 = esPotenciaDeDos (1)
+console.log(respuesta4)
+let respuesta5 = esPotenciaDeDos (0)
+console.log(respuesta5)
 module.exports = esPotenciaDeDos;
