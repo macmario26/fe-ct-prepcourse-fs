2621930d520e68cd324fc2a7a2785a63fa39afe9
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:52:15 -0300

    Update 07.js

--- M06 Bucles/ejercicios/07.js
+++ M06 Bucles/ejercicios/07.js
@@ -3,6 +3,10 @@ function esParYDivisiblePorTres(a) {
   // Retorna true si este es par y divisible por tres a la vez.
   // Retorna false si no lo es.
   // Tu código:
+  if (a%2 === 0 && a%3 === 0) {
+    console.log(true)
+  }else{console.log(false)}
 }
 
+esParYDivisiblePorTres(12)
 module.exports = esParYDivisiblePorTres;
