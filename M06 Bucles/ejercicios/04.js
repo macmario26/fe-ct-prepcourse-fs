463f443cedba4f4239a3a50e57de92781c7f9703
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:49:41 -0300

    Update 04.js

--- M06 Bucles/ejercicios/04.js
+++ M06 Bucles/ejercicios/04.js
@@ -3,6 +3,11 @@ function mayoriaDeEdad(edad) {
   // Si tiene 18 años o más, retorna el string: "Allowed".
   // Caso contrario: "Not allowed".
   // Tu código:
+
+  if (edad > 18) {
+    console.log('Allowed')
+  }else{console.log('Not allowed')}
 }
 
+mayoriaDeEdad(90)
 module.exports = mayoriaDeEdad;
