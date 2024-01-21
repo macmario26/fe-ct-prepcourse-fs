5b58a8363e98288c58820823fac9a98625f6af62
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:48:05 -0300

    Update 02.js

--- M06 Bucles/ejercicios/02.js
+++ M06 Bucles/ejercicios/02.js
@@ -2,6 +2,28 @@ function esVerdaderoYFalso(booleano1, booleano2) {
   // La función recibe dos argumentos "booleano1" y "booleano2".
   // Retorna true si ambos son verdaderos, caso contrario, retorna false.
   // Tu código:
+  return booleano1 && booleano2;
 }
 
+// Pasándole los parámetros
+let resultado1 = esVerdaderoYFalso(true, true);
+console.log("Resultado 1:", resultado1); // Salida esperada: true
+
+let resultado2 = esVerdaderoYFalso(true, false);
+console.log("Resultado 2:", resultado2); // Salida esperada: false
+
+let resultado3 = esVerdaderoYFalso(false, true);
+console.log("Resultado 3:", resultado3); // Salida esperada: false
+
+let resultado4 = esVerdaderoYFalso(false, false);
+console.log("Resultado 4:", resultado4); // Salida esperada: false
+
+
+//Analizando los tipos de datos
+let resultado5 = esVerdaderoYFalso(true, true);
+console.log("Resultado 5:", typeof resultado5); // Salida esperada: boolean
+let resultado6 = esVerdaderoYFalso(false, false);
+console.log("Resultado 6:", typeof resultado6); // Salida esperada: boolean
+
 module.exports = esVerdaderoYFalso;
+
