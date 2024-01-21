4417660da8cf66b291faf50a2767c14aa764f85d
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:53:31 -0300

    Update 09.js

--- M06 Bucles/ejercicios/09.js
+++ M06 Bucles/ejercicios/09.js
@@ -5,6 +5,17 @@ function conection(status) {
   // De lo contrario, presumimos que el usuario está "Offline".
   // Retornar el estado de conexión del usuario.
   // Tu código:
-}
+    if (status === 1) {
+      return 'Usuario Online'
+    } else if(status === 2){ return 'Usuario Away'
+      } else {return 'Usuario Offline'}
+  }
 
+
+let prueba1 = conection(1)
+console.log(prueba1);
+let prueba2 = conection(2)
+console.log(prueba2)
+let prueba0 = conection(0)
+console.log(prueba0)
 module.exports = conection;
