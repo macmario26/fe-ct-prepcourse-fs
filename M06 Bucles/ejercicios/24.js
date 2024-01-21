202482a9aac3138a537ee6273da51f1b393262f2
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 18:01:06 -0300

    Update 24.js

--- M06 Bucles/ejercicios/24.js
+++ M06 Bucles/ejercicios/24.js
@@ -2,6 +2,15 @@ function invertirTexto(texto) {
   // La función recibe un argumento "texto" que es un string.
   // Tu objetivo es invertir el string y devolver el string invertido.
   // Tu código:
+
+  let cc = texto.length;
+  console.log('cantidad de caracteres: '+cc);  
+
+  for (let inverso = cc - 1; inverso >= 0; inverso--) {
+    const elemento = texto[inverso];
+    console.log(elemento);
+  }
 }
 
+invertirTexto('mamma')
 module.exports = invertirTexto;
