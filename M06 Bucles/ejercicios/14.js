4acf8179bea15f07f3342a1459faea9a3f5993b8
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:56:44 -0300

    Update 14.js

--- M06 Bucles/ejercicios/14.js
+++ M06 Bucles/ejercicios/14.js
@@ -6,6 +6,27 @@ function operadoresLogicos(num1, num2, num3) {
   // Si todos los argumentos son cero, retorna ---> "Error".
   // Si no se cumple ninguna de las condiciones anteriores, retorna false.
   // Tu código:
+  switch (true) {
+    case (num1>0)&&(num1>num2)&&(num1>num3):
+      return'Número 1 es mayor y positivo'
+      
+    case (num1<0)||(num2<0)||(num3<0):
+      return'Hay negativos'
+      
+    case num3>num1 && num3>num2: num3++;
+      return num3
+           
+    case (num1==0)&&(num2==0)&&(num3==0):
+      return'Error'
+            
+    default: return false;
+      break;
+  }
 }
-
+let respuesta1 = operadoresLogicos(3,2,1)
+console.log(respuesta1)
+let respuesta2 = operadoresLogicos(0,0,0)
+console.log(respuesta2)
+let respuesta3 = operadoresLogicos(3,5,9)
+console.log(respuesta3)
 module.exports = operadoresLogicos;
