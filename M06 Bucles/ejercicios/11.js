545ff2ebfc4a2a3a8372f60a3797d9e135d3175c
Mario Maciel <156522381+macmario26@users.noreply.github.com>
2024-01-21 17:54:42 -0300

    Update 11.js

--- M06 Bucles/ejercicios/11.js
+++ M06 Bucles/ejercicios/11.js
@@ -2,6 +2,15 @@ function estaEnRango(num) {
   // Retorna true si "num" es menor que 50 y mayor que 20.
   // De lo contrario, retornar false.
   // Tu código:
+  if (num < 50 && num > 20) {
+    return true
+  } else{ return false }
 }
 
+let respuesta1 = estaEnRango(49)
+  console.log(respuesta1)
+  let respuesta2 = estaEnRango(50)
+  console.log(respuesta2)
+  let respuesta3 = estaEnRango(21)
+  console.log(respuesta3)
 module.exports = estaEnRango;
